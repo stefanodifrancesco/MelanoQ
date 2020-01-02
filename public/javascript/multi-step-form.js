f91170f4027baea023b6d57f90d71e2d0723e4de
Stefano Di Francesco <8914928+stefanodifrancesco@users.noreply.github.com>
2020-01-02 12:11:06 +0100

    Navbar restyle

--- public/javascript/multi-step-form.js
+++ public/javascript/multi-step-form.js
@@ -17,7 +17,7 @@ $(".scrollingdiv").on("scroll", function() {
     }
 });
 
-$(".start").click(function() {
+$("#confirmbutton").click(function() {
 
     $("#msform").validate({
         errorClass: 'invalid',
@@ -35,6 +35,62 @@ $(".start").click(function() {
     if ((!$('#msform').valid())) {
         return true;
     }
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({ opacity: 0 }, {
+        step: function(now, mx) {
+            //as the opacity of current_fs reduces to 0 - stored in "now"
+            //1. scale current_fs down to 80%
+            scale = 1 - (1 - now) * 0.2;
+            //2. bring next_fs from the right(50%)
+            left = (now * 50) + "%";
+            //3. increase opacity of next_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({
+                'transform': 'scale(' + scale + ')',
+                'position': 'absolute'
+            });
+            next_fs.css({ 'left': left, 'opacity': opacity });
+        },
+        duration: 600,
+        complete: function() {
+            current_fs.hide();
+            animating = false;
+
+            scrollerdiv = $(".scrollingdiv:visible");
+            if (scrollerdiv.nextAll('.action-button').css('display') == 'none') {
+                if (scrollerdiv.prop('scrollHeight') == scrollerdiv.prop('clientHeight')) {
+                    scrollerdiv.nextAll(".action-button").fadeIn(200, function() {
+                        scrollerdiv.nextAll(".action-button").css("display", "inline-block");
+                    });
+                } else {
+                    scrollerdiv.nextAll(".scroll-button-bottom").fadeIn(200, function() {
+                        scrollerdiv.nextAll(".scroll-button-bottom").css("display", "block");
+                    });
+                }
+            }
+
+
+        },
+        //this comes from the custom easing plugin
+        easing: 'easeInOutBack'
+    });
+
+});
+
+$("#fillbutton").click(function() {
+
+    $(".profile-header").css("display", "none");
+    $(".progressline").css("display", "block");
+
+
     if (animating) return false;
     animating = true;
 
