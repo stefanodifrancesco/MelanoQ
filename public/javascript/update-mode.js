7a42d7b25e45946ee030989ea9f62fdfe692cd04
Stefano Di Francesco <8914928+stefanodifrancesco@users.noreply.github.com>
2020-02-01 16:16:46 +0100

    Update mode SNL trigger added

--- public/javascript/update-mode.js
+++ public/javascript/update-mode.js
@@ -1102,25 +1102,34 @@ function Initialize() {
     $("[name='medical_history[pregnancy][melanoma_before_pregnancy][presence]']:checked").trigger('click');
     $("[name='medical_history[pregnancy][melanoma_after_pregnancy][presence]']:checked").trigger('click');
     $("[name='section_C_evaluation[difficult][answer]']").trigger('change');
+
+    /*** 1st primary melanoma characteristics ***/
     $("[name='melanoma_characteristics[first_melanoma][lesion][presence]']").trigger('change');
     $("[name='melanoma_characteristics[first_melanoma][detection]']").trigger('change');
     $("[name='melanoma_characteristics[first_melanoma][known][presence]']").trigger('change')
+    $("[name='melanoma_characteristics[first_melanoma][SNL][presence]']").trigger('change');
     $("[name='melanoma_characteristics[first_melanoma][multiple_primary][presence]']").trigger('change');
     $("[name='melanoma_characteristics[first_melanoma][known][histopathologic_features][regression][presence]']").trigger('change');
     $("[name='melanoma_characteristics[first_melanoma][known][histopathologic_features][nevus][presence]']").trigger('change');
+
+    /*** 2nd primary melanoma characteristics ***/
     $("[name='melanoma_characteristics[second_melanoma][lesion][presence]']").trigger('change');
     $("[name='melanoma_characteristics[second_melanoma][detection]']").trigger('change');
     $("[name='melanoma_characteristics[second_melanoma][known][presence]']").trigger('change')
+    $("[name='melanoma_characteristics[second_melanoma][SNL][presence]']").trigger('change');
     $("[name='melanoma_characteristics[second_melanoma][multiple_primary][presence]']").trigger('change');
     $("[name='melanoma_characteristics[second_melanoma][known][histopathologic_features][regression][presence]']").trigger('change');
     $("[name='melanoma_characteristics[second_melanoma][known][histopathologic_features][nevus][presence]']").trigger('change');
+
+    /*** 3rd primary melanoma characteristics ***/
     $("[name='melanoma_characteristics[third_melanoma][lesion][presence]']").trigger('change');
     $("[name='melanoma_characteristics[third_melanoma][detection]']").trigger('change');
     $("[name='melanoma_characteristics[third_melanoma][known][presence]']").trigger('change')
+    $("[name='melanoma_characteristics[third_melanoma][SNL][presence]']").trigger('change');
     $("[name='melanoma_characteristics[third_melanoma][multiple_primary][presence]']").trigger('change');
     $("[name='melanoma_characteristics[third_melanoma][known][histopathologic_features][regression][presence]']").trigger('change');
     $("[name='melanoma_characteristics[third_melanoma][known][histopathologic_features][nevus][presence]']").trigger('change');
-    /****** Trigger change on all input of type select with an hidden control ******/
+
 };
 
 function Reset_Values(tag_fieldset) {
